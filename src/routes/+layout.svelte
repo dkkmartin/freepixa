af323b12e80718a7b930eb6c7694c666ebd38684
Martin <martinbruun1995@gmail.com>
2024-04-26 18:14:43 +0200

    Add shadnc and tailwindcss

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+	import '../app.pcss';
+	import { ModeWatcher } from 'mode-watcher';
+</script>
+
+<main class="h-screen">
+	<slot></slot>
+</main>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,12 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	import { Switch } from '$lib/components/ui/switch';
+	import { Label } from '$lib/components/ui/label';
+	import { toggleMode } from 'mode-watcher';
+</script>
+
+<h1>Hey</h1>
+
+<div class="flex items-center space-x-2">
+	<Switch on:click={toggleMode} id="theme-mode"></Switch>
+	<Label for="theme-mode">Theme</Label>
+</div>
