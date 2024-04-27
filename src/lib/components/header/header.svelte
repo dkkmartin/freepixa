6218861a63cfc216d05648f2c805381c52b5e785
Martin <martinbruun1995@gmail.com>
2024-04-27 19:22:24 +0200

    Add header component with theme toggler

---
+++ src/lib/components/header/header.svelte
@@ -0,0 +1,14 @@
+<script>
+	import ThemeToggler from './themeToggler.svelte';
+</script>
+
+<header class="flex items-center justify-between border-b p-2 shadow-sm">
+	<h1 class="heading-title text-5xl">FreePixa</h1>
+	<ThemeToggler></ThemeToggler>
+</header>
+
+<style>
+	.heading-title {
+		font-family: 'Jersey 25', sans-serif;
+	}
+</style>
