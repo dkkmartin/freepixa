6fc53c968ee12ec1f78b13a3f47e7dfdf48e450b
Martin <martinbruun1995@gmail.com>
2024-04-29 13:52:16 +0200

    Add new SVG icons and badge component to UI library, update image page layout

--- src/routes/image/[slug]/+page.svelte
+++ src/routes/image/[slug]/+page.svelte
@@ -1,8 +1,11 @@
 <script lang="ts">
 	import * as Avatar from '$lib/components/ui/avatar/index';
+	import { Badge } from '$lib/components/ui/badge/';
+	import { Button } from '$lib/components/ui/button/';
 	import { Image } from '@unpic/svelte';
 	import { imageDataStore } from '$lib/stores/stores';
-	import MaterialSymbolsLocationOn from '$lib/components/svg/MaterialSymbolsLocationOn.svelte';
+	import Location from '$lib/components/svg/MaterialSymbolsLocationOn.svelte';
+	import Calendar from '$lib/components/svg/MaterialSymbolsLightCalendarTodayOutline.svelte';
 	import { type Result } from '$lib/types/unsplash';
 
 	let data: Result;
@@ -10,28 +13,55 @@
 		data = $imageDataStore as Result;
 		console.log(data);
 	}
+
+	function formatDate(dateString: string) {
+		const date = new Date(dateString);
+		return date.toLocaleDateString(undefined, { year: 'numeric', month: 'long', day: 'numeric' });
+	}
 </script>
 
-<section class="flex flex-col gap-4 py-4">
-	<div class="flex flex-col gap-2">
+<article class="flex flex-col gap-4 py-4">
+	<header class="flex flex-col gap-2">
 		<div class="flex items-center justify-between px-2">
-			<a href={data.user.links.html} target="_blank">
+			<a
+				href={data.user.links.html}
+				target="_blank"
+				rel="noopener noreferrer"
+				aria-label="User profile"
+			>
 				<div class="flex items-center gap-2">
 					<Avatar.Root>
-						<Avatar.Image src={data.user.profile_image.small}></Avatar.Image>
+						<Avatar.Image src={data.user.profile_image.small} alt="User avatar"></Avatar.Image>
 						<Avatar.Fallback>FP</Avatar.Fallback>
 					</Avatar.Root>
 					<b>{data.user.name}</b>
 				</div>
 			</a>
-			<div class="flex gap-2">
-				<MaterialSymbolsLocationOn></MaterialSymbolsLocationOn>
+			<div class="flex items-center gap-2">
+				<Location></Location>
 				<p class="text-sm text-muted-foreground">{data.user.location}</p>
 			</div>
 		</div>
 		<Image layout="fullWidth" src={data.urls.small} alt={data.alt_description} />
-		<div class="px-2">
+		<div class="flex items-center justify-between px-2">
 			<small class="text-sm font-medium leading-none">Likes: {data.likes}</small>
+			<div class="flex items-center gap-2">
+				<Calendar></Calendar>
+				<p class="text-sm text-muted-foreground">{formatDate(data.created_at)}</p>
+			</div>
 		</div>
+	</header>
+</article>
+
+<section class="flex justify-between p-2">
+	<div class="flex flex-wrap gap-2">
+		{#each data.tags as tag}
+			<a href={'/search/' + tag.title}>
+				<Badge>{tag.title}</Badge>
+			</a>
+		{/each}
 	</div>
+	<a target="_blank" rel="noopener noreferrer" aria-label="Download" href={data.links.download}
+		><Button>Download</Button></a
+	>
 </section>
