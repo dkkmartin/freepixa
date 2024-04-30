a59848eeeafa608c4dd24440f0060093c02bfd64
Martin <martinbruun1995@gmail.com>
2024-04-30 12:36:54 +0200

    Update search page images with blur_hash implementation

--- src/routes/search/[slug]/+page.svelte
+++ src/routes/search/[slug]/+page.svelte
@@ -1,26 +1,21 @@
 <script lang="ts">
 	import { Image } from '@unpic/svelte';
 	import { type UnsplashData } from '$lib/types/unsplash';
-	import * as Pagination from '$lib/components/ui/pagination';
 	import * as Avatar from '$lib/components/ui/avatar/index';
 	import { imageDataStore } from '$lib/stores/stores';
 	import { Skeleton } from '$lib/components/ui/skeleton';
-	import { mediaQuery } from 'svelte-legos';
+	import SvgSpinners3DotsMove from '$lib/components/svg/SvgSpinners3DotsMove.svelte';
+	import { infiniteScrollAction } from 'svelte-legos';
 	import { goto } from '$app/navigation';
+	import { blurhashToCssGradientString } from '@unpic/placeholder';
 
 	export let data: UnsplashData;
 
-	console.log(data);
+	let isLoading = false;
 	let results = data.data.response.results;
 	let capitalized = data.params.charAt(0).toUpperCase();
 	let imageLoaded: { [key: string]: boolean } = {};
-	let count: number;
-	let currentPageNumber: number;
-
-	const isDesktop = mediaQuery('(min-width: 768px)');
-
-	$: perPage = $isDesktop ? 2 : 10;
-	$: siblingCount = $isDesktop ? 2 : 0;
+	let pageNumber: number = 1;
 
 	function handleImageLoad(imageId: string) {
 		imageLoaded[imageId] = true;
@@ -31,27 +26,45 @@
 		imageDataStore.set(selectedImage);
 	}
 
-	function handlePage(pageNumber: number) {
-		currentPageNumber = pageNumber;
+	function blurhashPlaceholder(blurhash: string) {
+		const placeholder = blurhashToCssGradientString(blurhash);
+		return placeholder;
+	}
+
+	function handlePage() {
 		// Create a new URLSearchParams object
 		let params = new URLSearchParams(window.location.search);
-
 		// Set the 'page' parameter to the new page number
 		params.set('page', String(pageNumber));
-
 		// Navigate to the new URL
 		goto('?' + params.toString());
+	}
+
+	function loadItems() {
+		if (!isLoading) return;
+		isLoading = true;
+		pageNumber += 1;
+		handlePage();
+		console.log(pageNumber);
+		isLoading = false;
 	}
 
 	$: {
 		results = data.data.response.results;
 		capitalized = data.params.charAt(0).toUpperCase();
-		count = data.data.response.total;
 	}
 </script>
 
 <h1 class="px-2 py-4 text-3xl">Results for: {capitalized + data.params.slice(1)}</h1>
-<section class="flex flex-col gap-4 py-4">
+
+<section
+	class="flex flex-col gap-4 py-4"
+	use:infiniteScrollAction={{
+		delay: 200,
+		distance: 0,
+		cb: loadItems
+	}}
+>
 	{#each results as imageObject (imageObject.id)}
 		<div class="flex flex-col gap-2">
 			<div class="flex items-center gap-2 px-2">
@@ -75,45 +88,23 @@
 						src={imageObject.urls.small}
 						alt={data.params}
 						on:load={() => handleImageLoad(imageObject.id)}
+						background={blurhashPlaceholder(imageObject.blur_hash)}
 					/>
 				</div>
+
 				{#if !imageLoaded[imageObject.id]}
-					<Skeleton class="h-[400px] w-full" />
+					<Skeleton
+						class="h-[400px] w-full object-cover"
+						style={'background: ' + blurhashPlaceholder(imageObject.blur_hash)}
+					/>
 				{/if}
 			</a>
 		</div>
 	{/each}
+	{#if isLoading}
+		<SvgSpinners3DotsMove></SvgSpinners3DotsMove>
+	{/if}
 </section>
-
-<Pagination.Root class="py-4" {count} {perPage} {siblingCount} let:pages let:currentPage>
-	<Pagination.Content>
-		<Pagination.Item>
-			<Pagination.PrevButton on:click={() => handlePage((currentPage || 0) - 1)}
-			></Pagination.PrevButton>
-		</Pagination.Item>
-		{#each pages as page (page.key)}
-			{#if page.type === 'ellipsis'}
-				<Pagination.Item>
-					<Pagination.Ellipsis />
-				</Pagination.Item>
-			{:else}
-				<Pagination.Item>
-					<Pagination.Link
-						{page}
-						isActive={currentPage === page.value}
-						on:click={() => handlePage(page.value)}
-					>
-						{page.value}
-					</Pagination.Link>
-				</Pagination.Item>
-			{/if}
-		{/each}
-		<Pagination.Item>
-			<Pagination.NextButton on:click={() => handlePage((currentPage || 0) + 1)}
-			></Pagination.NextButton>
-		</Pagination.Item>
-	</Pagination.Content>
-</Pagination.Root>
 
 <style>
 	.image-visible {
