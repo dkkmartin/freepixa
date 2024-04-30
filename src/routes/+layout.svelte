45d2ea1313af3ecbf3389b5c9b48ccac4d5f1159
Martin <martinbruun1995@gmail.com>
2024-04-30 18:33:36 +0200

    Add Netlify configuration file and update package dependencies

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,12 +5,21 @@
 </script>
 
 <svelte:head>
+	<title>FreePixa</title>
+	<meta name="description" content="Search billions of images" />
 	<link rel="preconnect" href="https://fonts.googleapis.com" />
 	<link rel="preconnect" href="https://fonts.gstatic.com" />
 	<link
 		href="https://fonts.googleapis.com/css2?family=Jersey+25&family=Roboto+Condensed:ital,wght@0,100..900;1,100..900&display=swap"
 		rel="stylesheet"
 	/>
+	<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+	<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+	<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+	<link rel="manifest" href="/site.webmanifest" />
+	<link rel="mask-icon" href="/safari-pinned-tab.svg" color="#000000" />
+	<meta name="msapplication-TileColor" content="#da532c" />
+	<meta name="theme-color" content="#ffffff" />
 </svelte:head>
 
 <ModeWatcher />
