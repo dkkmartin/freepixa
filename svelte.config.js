fbd4d075a19faca00c258aa492a0073a4cde0c21
Martin <martinbruun1995@gmail.com>
2024-05-03 22:24:56 +0200

    Prerender test

--- svelte.config.js
+++ svelte.config.js
@@ -3,14 +3,17 @@ import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
-	// for more information about preprocessors
 	preprocess: [vitePreprocess({})],
-
 	kit: {
-		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
-		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
-		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
+		prerender: {
+			handleHttpError: ({ path, message }) => {
+				if (path === '/favicon.png') {
+					return; // Ignore the 404 error for the favicon.png file
+				}
+
+				throw new Error(message); // Otherwise fail the build
+			}
+		},
 		adapter: adapter({
 			edge: true
 		})
